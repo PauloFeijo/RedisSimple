ffc3a2a3ce952232808da8a77f27d229827b52af
PauloFeijo <paulorikrdo.89@gmail.com>
2021-09-28 15:48:31 -0300

    Simple Redis example

---
+++ Entities/Customer.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace RedisSimple.Entities
+{
+    public class Customer
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public string Phone { get; set; }
+        public string Address { get; set; }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,58 @@
+﻿using RedisSimple.Entities;
+using ServiceStack.Redis;
+using System;
+
+namespace RedisSimple
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var customer = new Customer()
+            {
+                Id = Guid.NewGuid(),
+                Name = "Paul",
+                Address = "Street blue bird, 457",
+                Phone = "1234567489"
+            };
+
+            // Create
+            SaveCustomerOnRedis(customer);
+
+            // Get
+            var customer2 = GetCustomerFromRedis(customer.Id.ToString());
+
+            // Update
+            customer2.Name = "Joe";
+            SaveCustomerOnRedis(customer2);
+
+            // Delete
+            DeleteCustomerOnRedis(customer2);
+        }
+
+        static void SaveCustomerOnRedis(Customer customer)
+        {
+            CreateRedisClient().Set<Customer>(customer.Id.ToString(), customer);
+            Console.WriteLine($"Customer {customer.Name} saved on redis");
+        }
+
+        static Customer GetCustomerFromRedis(string customerId)
+        {
+            var customer = CreateRedisClient().Get<Customer>(customerId);
+            Console.WriteLine($"Customer {customer.Name} get from redis");
+            return customer;
+        }
+
+        static void DeleteCustomerOnRedis(Customer customer)
+        {
+            CreateRedisClient().Expire(customer.Id.ToString(), 0);
+            Console.WriteLine($"Customer {customer.Name} deleted on redis");
+        }
+
+        private static RedisClient CreateRedisClient()
+        {
+            var host = "localhost:6379";
+            return new RedisClient(host);
+        }
+    }
+}
